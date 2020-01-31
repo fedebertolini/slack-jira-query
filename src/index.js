1f971f309279ba70b1be4794517c10648eee5628
Federico Bertolini <fb@hellofresh.com>
2020-01-31 09:35:40 +0100

    dont send report if query returns no items

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ const { buildBlocks, sendSlackMessage } = require('./slack');
 (async () => {
     try {
         const result = await executeQuery();
-        const blocks = buildBlocks(result);
-        await sendSlackMessage(blocks);
+        if (result.total > 0) {
+            const blocks = buildBlocks(result);
+            await sendSlackMessage(blocks);
+        } else {
+            console.log('No issues in query result');
+        }
     } catch (err) {
         console.error(err);
     }
